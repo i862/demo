413a58f7fe19bcbe5be844027c50963b153bdc53
amenema <men_zhong_xin@163.com>
2016-03-03 23:15:39 +0800

    fix bug

--- app.js
+++ app.js
@@ -1,45 +1,16 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+var favicon = require('serve-favicon'),
+    serveStatic = require('serve-static')
+  , bodyParser = require('body-parser')
+  , jsonParser = bodyParser.json();
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
 var app = express();
-
-app.get("/",function(req,res){
-  render('hello!');
-});
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+//app.use(serveStatic(__dirname + '/views'));
+app.use(serveStatic(__dirname + '/assets'));
+app.use(jsonParser);
 
 
 exports.server = app;
