0d24560d0992cd925a79a1f9b9c0a83e655fc4c9
amenema <men_zhong_xin@163.com>
2016-03-04 11:43:58 +0800

    fix bug

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.get('/test',function(req,res){
 app.get('/',function(req,res){
   res.send('ok');
 });
-function run_cmd(cmd, args, callback) {
+function run_cmd(callback) {
   callfile.execFile((__dirname + '/bash/autoPull.sh'),function (err, stdout, stderr) {
     callback(err, stdout, stderr);
   });
@@ -36,6 +36,9 @@ handler.on('push', function (event) {
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
     event.payload.ref);
+  run_cmd(function(err){
+    console.log(err);
+  });
 })
 
 exports.server = app;
