a9f4d2ff1b606e671a56945262ea498398d6e69c
amenema <men_zhong_xin@163.com>
2016-03-04 11:55:56 +0800

    fix

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(jsonParser);
 
 app.get('/test',function(req,res){
   console.log(req.headers);
-  res.send('hello world amen');
+  res.send('hello world amen' + Date.now());
 });
 app.post('/git/auto',function(req,res){
   callfile.execFile((__dirname + '/bash/autoPull.sh'),function (err, stdout, stderr) {
