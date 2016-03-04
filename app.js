8d7d8323662403d8bb952241c79ea2878a6361ce
amenema <men_zhong_xin@163.com>
2016-03-04 17:52:17 +0800

    f

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var favicon = require('serve-favicon')
   , logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var callfile = require('child_process');
-
+var crypto = require('crypto');
 
 
 var app = express();
@@ -23,16 +23,17 @@ app.get('/test',function(req,res){
   res.send('helloe woereldee 1ee=e' + Date.now());
 });
 app.post('/git/auto',function(req,res){
+  var hmac = crypto.createHmac('sha1','amenema').update(req.body).digest();
   callfile.execFile((__dirname + '/bash/autoPull.sh'),function (err, stdout, stderr) {
     callback(err, stdout, stderr);
   });
 
+  console.log(hmac);
   console.log('=====================');
   console.log(req.headers);
   console.log('ref:  '+req.body.ref);
   console.log('serverId:  '+req.body.repository.id);
   console.log('serverName:  '+req.body.repository.name);
-  console.log(req.body.hook);
   console.log('=====================');
   res.send('ok');
 });
