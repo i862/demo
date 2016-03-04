bd30cd677fe2aa44d4671356a81eb299c06fa278
amenema <men_zhong_xin@163.com>
2016-03-04 10:10:17 +0800

    test

--- app.js
+++ app.js
@@ -3,14 +3,26 @@ var path = require('path');
 var favicon = require('serve-favicon'),
     serveStatic = require('serve-static')
   , bodyParser = require('body-parser')
+  , ejs = require('ejs')
   , jsonParser = bodyParser.json();
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 
 var app = express();
+app.engine('.html', ejs.__express);
+app.set('view engine', 'html');
 //app.use(serveStatic(__dirname + '/views'));
-app.use(serveStatic(__dirname + '/assets'));
+//app.use(serveStatic(__dirname + '/assets'));
 app.use(jsonParser);
+
+app.post('/git/auto/',function(req,res){
+  console.log("===================================");
+  console.log(req.body);
+  console.log("===================================");
+  console.log(req.headers.secret);
+  console.log(req.headers);
+  res.send('ok');
+});
 
 
 exports.server = app;
