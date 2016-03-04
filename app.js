55cd6098c9f11fa244676e286a8a49d64c093ba5
amenema <men_zhong_xin@163.com>
2016-03-04 15:47:55 +0800

    f

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.post('/git/auto',function(req,res){
   callfile.execFile((__dirname + '/bash/autoPull.sh'),function (err, stdout, stderr) {
     callback(err, stdout, stderr);
   });
+
   console.log('=====================');
   console.log(req.headers);
   console.log('ref:  '+req.body.ref);
